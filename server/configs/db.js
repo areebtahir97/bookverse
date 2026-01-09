c2ce966189c51b15f251208840df16afac931d36
Areeb Tahir <areebtahir97@gmail.com>
2026-01-09 22:58:08 +0530

    frontend production updates

--- server/configs/db.js
+++ server/configs/db.js
@@ -6,7 +6,11 @@ const db = new Pool({
   host:  process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: 5432,
+  port:  Number(process.env.DB_PORT) || 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
 });
 
 export default db;
