86f2a33ab17642460c4899cb94041f73e759c602
Areeb Tahir <areebtahir97@gmail.com>
2026-01-09 23:07:07 +0530

    frontend production updates

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,5 +1,9 @@
 import pkg from "pg";
 const { Pool } = pkg;
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 const db = new Pool({
   user: process.env.DB_USER,
